1686d00f6f60311ceca950ae6a6c00804851328f
Nightsky_Mac <bluebarryteam@gmail.com>
2024-01-13 16:39:01 +0900

    fix(api): resolve node-fetch security vulnerability

--- pages/api/getvalid.ts
+++ pages/api/getvalid.ts
@@ -1,18 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import fetch from 'node-fetch'
-
+import axios from "axios"
 export default function handler(req: NextApiRequest,res: NextApiResponse
-    ) {
+) {
         const Id = req.query.id
-        fetch(`https://bjapi.afreecatv.com/api/${Id}/station`, {headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Whale/3.22.205.18 Safari/537.36'}})
-            .then((response) => {
-                if (response.status === 515){
-                    res.send(false)
-                }
-                else{
-                    res.send(true)
-                }
-            })
-            
-            
+        axios.get(`https://bjapi.afreecatv.com/api/${Id}/station`, { headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Whale/3.22.205.18 Safari/537.36'}})
+        .then((response) => {
+            if(response.status === 515){
+                res.send(false);
+            } else {
+                res.send(true);
+            }
+        })
   }
