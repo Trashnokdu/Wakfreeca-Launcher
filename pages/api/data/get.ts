679936fc39a61e33797e2f51f6a66f64bbaad5b1
Trashnokdu <1023aass@naver.com>
2024-02-09 13:44:47 +0900

    please last

--- pages/api/data/get.ts
+++ pages/api/data/get.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 const mysql = require("mysql")
 import { getToken, decode } from "next-auth/jwt";
+import { getSession } from 'next-auth/react';
 require('dotenv').config();
 const secret = process.env.Secret;
 const pool = mysql.createPool({
@@ -20,16 +21,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 console.error('Error connecting: ' + err.stack);
                 return res.status(500).send("500 Internal Server Error");
             }
-            const session = await getToken({ req, secret, raw: true });
+            const session = await getSession({ req })
             if(!session){
                 res.status(401).send("401 Unauthorized")
             }
             try{
-                const decoded = await decode({
-                    token: session,
-                    secret: secret ? secret : '',
-                  });
-                const email = decoded?.email
+                const email = session?.user?.email
                 if(!email){
                     return res.status(500).send("500 Internal Server Error")
                 }
